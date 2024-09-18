0a2ac8b48a2e14c04694f0bfb4f21744f69a8521
Jayeeh <erroljustinecasbadillo23@gmail.com>
2024-09-19 02:40:45 +0800

    Initialize Online Text Editor - Basic Sample

---
+++ script.js
@@ -0,0 +1,28 @@
+let el = document.querySelector("#words");
+
+function setColor() {
+  let input = document.querySelectorAll("input")[0];
+  el.style.color = input.value;
+}
+
+function toggleUnderline() {
+  el.classList.toggle("underline");
+}
+
+function toggleItalic() {
+  el.classList.toggle("italic");
+}
+
+function toggleBold() {
+  el.classList.toggle("bold");
+}
+
+function toggleMonospace() {
+  el.classList.toggle("mono");
+}
+
+function removeFormat() {
+  el.classList.remove("underline", "italic", "bold", "mono");
+  el.style.color = "black";
+}
+
